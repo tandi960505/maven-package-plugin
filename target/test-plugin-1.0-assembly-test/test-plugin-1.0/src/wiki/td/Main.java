042233b0b210ec36e6c220cd83903d605cb17446
tandi <13076619353@163.com>
2019-07-26 14:48:29 +0800

    first commit

---
+++ target/test-plugin-1.0-assembly-test/test-plugin-1.0/src/wiki/td/Main.java
@@ -0,0 +1,10 @@
+package wiki.td;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("正在运行：{}", Main.class);
+    }
+}
